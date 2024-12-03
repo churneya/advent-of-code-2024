d85ce66de4ae6819514c7d47f6c06ba695f53540
sergei.smirnov <sergei.smirnov@poleinvest.ru>
2024-12-03 22:36:26 +0300

    add empty day

---
+++ src/Day00.kt
@@ -0,0 +1,22 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // Test if implementation meets criteria from the description, like:
+//    check(part1(listOf("test_input")) == 1)
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day00_test")
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day00")
+    part1(input).println()
+    part2(input).println()
+}

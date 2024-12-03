34ba8d2b084ed8b172a30718eaae2d569c404004
sergei.smirnov <sergei.smirnov@poleinvest.ru>
2024-12-03 22:31:13 +0300

    day 1 - part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
         val left = mutableListOf<Int>()
@@ -11,19 +13,27 @@ fun main() {
         right.sort()
         var sum = 0
         for (i in left.indices) {
-            sum += if(left[i] > right[i]) {
-                left[i] - right[i]
-            } else {
-                right[i] - left[i]
-            }
+            sum += abs(left[i] - right[i])
         }
 
         return sum
     }
 
-//    fun part2(input: List<String>): Int {
-//        return input.size
-//    }
+    fun part2(input: List<String>): Int {
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+        input.map { s ->
+            val (l, r) = s.split("   ").map { it.toInt() }
+            left.add(l)
+            right.add(r)
+        }
+        var sum = 0
+
+        left.forEach { l ->
+            sum += right.count { it == l } * l
+        }
+        return sum
+    }
 
     // Test if implementation meets criteria from the description, like:
 //    check(part1(listOf("test_input")) == 1)
@@ -31,9 +41,10 @@ fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
